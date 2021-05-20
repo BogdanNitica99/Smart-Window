e0ba1a4a77b95d38db93ec5ceecc2b5aa2d5200e
CarlaMaria23 <enachecarla43@gmail.com>
2021-05-20 21:12:47 +0300

    Add log function

--- server.cpp
+++ server.cpp
@@ -1,30 +1,41 @@
 #include <string>
 #include <pistache/endpoint.h>
 #include <pistache/router.h>
-
-
+#include <fstream>
+ 
 using namespace std;
 using namespace Pistache;
 using namespace Pistache::Rest;
-
+ 
 int tempInside, tempOutside, humInside, humOutside, lightInside, lightOutside;
 int windowOpenness = 0, curtainOpenness = 100, wantedTemp = 19, wantedHumidity = 40, wantedLight = 7500;
+ 
+ void log( string text ){
+    string filename("log.txt");
+    ofstream file_out;
+ 
+    file_out.open(filename, std::ios_base::app);
+    file_out << text << endl;
+    file_out.close();   
+}
+ 
 
 bool is_digits(const std::string &str) {
     return str.find_first_not_of("0123456789") == std::string::npos;
 }
-
+ 
 void testRoute(const Rest::Request& request, Http::ResponseWriter response) {
     cout << "Received a request from IP address " << request.address().host() << '\n';
-
+ 
     response.send(Http::Code::Ok, "Test Route Ok!\n");
 } 
-
+ 
 void setSensorValue(const Rest::Request& request, Http::ResponseWriter response) {
+    log("setSensorValue");
     auto location = request.param(":location").as<std::string>();
     auto option = request.param(":option").as<std::string>();
     auto value = request.param(":value").as<std::string>();
-
+ 
     if(location.compare("outside") != 0 && location.compare("inside") != 0) {
         response.send(Http::Code::Not_Found, "Location poate avea doar valorile outside sau inside.\n");
         return;
@@ -37,7 +48,7 @@ void setSensorValue(const Rest::Request& request, Http::ResponseWriter response)
         response.send(Http::Code::Not_Found, "Value poate fi doar un numar intreg.\n");
         return;
     }
-
+ 
     if(location.compare("inside") == 0) {
         if(option.compare("temperature") == 0) {
             tempInside = stoi(value);
@@ -60,14 +71,15 @@ void setSensorValue(const Rest::Request& request, Http::ResponseWriter response)
             lightOutside = stoi(value);
         }
     }
-    
+ 
     response.send(Http::Code::Ok, "Sensor settings saved\n");
 }
-
+ 
 void getSensorValue(const Rest::Request& request, Http::ResponseWriter response) {
+    log("getSensorValue");
     auto location = request.param(":location").as<std::string>();
     auto option = request.param(":option").as<std::string>();
-
+ 
     if(location.compare("outside") != 0 && location.compare("inside") != 0) {
         response.send(Http::Code::Not_Found, "Location poate avea doar valorile outside sau inside.\n");
         return;
@@ -76,7 +88,7 @@ void getSensorValue(const Rest::Request& request, Http::ResponseWriter response)
         response.send(Http::Code::Not_Found, "Option poate avea doar valorile temperature, humidity sau light.\n");
         return;
     }
-
+ 
     if(location.compare("inside") == 0) {
         if(option.compare("temperature") == 0) {
             response.send(Http::Code::Ok, location + " + " + option + " = " + to_string(tempInside) + "\n");
@@ -100,21 +112,23 @@ void getSensorValue(const Rest::Request& request, Http::ResponseWriter response)
         }
     }
 }
-
+ 
+ 
 void setWantedValues(const Rest::Request& request, Http::ResponseWriter response) {
+    log("setWantedValues");
     auto option = request.param(":option").as<std::string>();
     auto value = request.param(":value").as<std::string>();
-
+ 
     if(option.compare("temperature") != 0 && option.compare("humidity") != 0 && option.compare("light") != 0) {
         response.send(Http::Code::Not_Found, "Option poate avea doar valorile temperature, humidity sau light.\n");
         return;
     }
-
+ 
     if(!is_digits(value)) {
         response.send(Http::Code::Not_Found, "Value poate fi doar un numar intreg.\n");
         return;
     }
-
+ 
     if(option.compare("temperature") == 0) {
         wantedTemp = stoi(value);
     }
@@ -124,18 +138,18 @@ void setWantedValues(const Rest::Request& request, Http::ResponseWriter response
     if(option.compare("light") == 0) {
         wantedLight = stoi(value);
     }
-
+ 
     response.send(Http::Code::Ok, "Wanted settings saved\n");
 }
-
+ 
 void getWantedValues(const Rest::Request& request, Http::ResponseWriter response) {
+    log("getWantedValues");
     auto option = request.param(":option").as<std::string>();
-
     if(option.compare("temperature") != 0 && option.compare("humidity") != 0 && option.compare("light") != 0) {
         response.send(Http::Code::Not_Found, "Option poate avea doar valorile temperature, humidity sau light.\n");
         return;
     }
-
+ 
     if(option.compare("temperature") == 0) {
         response.send(Http::Code::Ok, "Wanted " + option + " = " + to_string(wantedTemp) + "\n");
     }
@@ -146,59 +160,63 @@ void getWantedValues(const Rest::Request& request, Http::ResponseWriter response
         response.send(Http::Code::Ok, "Wanted " + option + " = " + to_string(wantedLight) + "\n");
     }
 }
-
+ 
 void giveCommand()
 {
+    log("giveCommand");
     if((humInside < wantedHumidity) && (humOutside >= wantedHumidity))
         windowOpenness += 50;
     if((humInside > wantedHumidity) && (humOutside <= wantedHumidity))
         windowOpenness += 50;
-
+ 
     if((tempInside < wantedTemp) && (tempOutside >= wantedTemp))
         windowOpenness += 50;
     if((tempInside > wantedTemp) && (tempOutside <= wantedTemp))
         windowOpenness += 50;
-
-
+ 
+ 
     if((humInside < wantedHumidity) && (humOutside < wantedHumidity))
         windowOpenness = 0;
     if((humInside > wantedHumidity) && (humOutside > wantedHumidity))
         windowOpenness = 0;
-    
+ 
     if((tempInside < wantedTemp) && (tempOutside < wantedTemp))
         windowOpenness = 0;
     if((tempInside > wantedTemp) && (tempOutside > wantedTemp))
         windowOpenness = 0;
-
+ 
     if((lightInside < wantedLight) && (lightOutside >= wantedLight))
         curtainOpenness = 100;
     if(lightInside > wantedLight)
         curtainOpenness -= 75;
-
+ 
     if(windowOpenness > 100)
         windowOpenness = 100;
     if(curtainOpenness < 0)
         curtainOpenness = 0;
 }
+ 
 
 int main() {
     Router router;
-
+ 
     // Ruta de test pentru libraria Pistache
     router.get("/test", Routes::bind(testRoute));
-
+ 
     // Rute pentru setarea si returnarea valorilor senzorilor din interior si exterior
     router.get("/sensor/set/:location/:option/:value", Routes::bind(setSensorValue));
     router.get("/sensor/get/:location/:option", Routes::bind(getSensorValue));
-
+ 
     // Rute pentru setarea si returnarea valorilor dorite in interiorul camerei
     router.get("/wantedValues/set/:option/:value", Routes::bind(setWantedValues));
     router.get("/wantedValues/get/:option", Routes::bind(getWantedValues));
+ 
 
+ 
     Address addr(Ipv4::any(), Port(9080));
-
+ 
     cout << "Serverul asteapta requesturi\n";
-
+ 
     auto opts = Http::Endpoint::options().threads(1);
     Http::Endpoint server(addr);
     server.init(opts);
